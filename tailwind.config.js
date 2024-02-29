ece4365f768490d1d13e0ea7b343a7c6a49b8266
yule <keyule@gmail.com>
2024-02-29 17:43:57 +0800

    Added colors/fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}",'./index.html'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'Rose-White': '#fff5fa',
+      'Eggshell': '#f3e6d8',
+      'Light-Grey': '#e4ded8',
+      'Wenge-Brown': '#5f574e',
+      'Dark-Charcoal': '#302d2c',
+      'Nutmeg': '#854632',
+      'Dark-Raspbery': '#7b284f',
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+      serif: ['Yong Serif', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
